a799cf9544b18d6366259f23ac49653355bb0977
wangfeng <578627759@qq.com>
2016-07-19 18:49:51 +0800

    [ wangfeng ] 实现了在view中调用Presenter中的打印和置空功能

--- src/main/java/View/PrintBill.java
+++ src/main/java/View/PrintBill.java
@@ -1,7 +1,18 @@
 package View;
 
+import Presenter.OutPutBill;
+import Presenter.ResetBillEmpty;
+
 /**
  * Created by feng on 2016/7/17.
  */
 public class PrintBill {
+
+    public PrintBill() {
+        OutPutBill print = new OutPutBill();
+        ResetBillEmpty reset = new ResetBillEmpty();
+        print.printFullBill();
+        reset.resetAll();
+    }
+
 }
