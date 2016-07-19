98a36cf0effdad03b410e1c45d24c27201a7cecb
hetianguang <hetianguang123@163.com>
2016-07-19 10:19:43 +0800

    [tianguang] 更改CalculatePriceTest类中的代码：清除测试产生的底层数据

--- src/test/java/Presenter/CalculatePriceTest.java
+++ src/test/java/Presenter/CalculatePriceTest.java
@@ -3,6 +3,7 @@ package Presenter;
 import Model.CommodityRepertory;
 import Model.PrintBillDetails.ShoppingListAll;
 import Model.PrintBillDetails.SumOfBill;
+import Model.ShoppingList;
 import Presenter.DataInput.InputCommodityInfo;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,7 +25,7 @@ public class CalculatePriceTest {
         String goods1 = "ITEM000021 大豆 斤 食品 8.00 无";
         String goods2 = "ITEM000022 可口可乐 瓶 饮料 3.00 买二赠一";
         String goods3 = "ITEM000023 苹果 斤 水果 9.00 九五";
-        String goods4 = "ITEM000024 纸巾 盒 日用品 3.50 买二赠一-九五";
+        String goods4 = "ITEM000024 纸巾 盒 日用品 3.50 买二赠一_九五";
         inputCommodityInfo.insertNewCommInfo(goods1);
         inputCommodityInfo.insertNewCommInfo(goods2);
         inputCommodityInfo.insertNewCommInfo(goods3);
@@ -43,6 +44,9 @@ public class CalculatePriceTest {
         //then
         assertThat(SumOfBill.total, is(6.00));
         assertThat(SumOfBill.privilege, is(0.00));
+        CommodityRepertory.commodityInfomap.clear();
+        ShoppingList.shoppinglist.clear();
+
     }
 
 
