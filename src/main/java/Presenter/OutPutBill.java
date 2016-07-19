f8cd720ac0e6751303fe7693e9b4c6d369db0496
wangfeng <578627759@qq.com>
2016-07-19 18:18:16 +0800

    [ wangfeng ] 实现了打印小票中含有买二赠一的情况

--- src/main/java/Presenter/OutPutBill.java
+++ src/main/java/Presenter/OutPutBill.java
@@ -1,7 +1,78 @@
 package Presenter;
 
+import Model.PrintBillDetails.PrivilegeThreeforTwo;
+import Model.PrintBillDetails.ShoppingListAll;
+import Model.PrintBillDetails.SumOfBill;
+
 /**
  * Created by feng on 2016/7/17.
  */
 public class OutPutBill {
+
+    ShoppingListAll shoppingList = new ShoppingListAll();
+    PrivilegeThreeforTwo threeforTwo = new PrivilegeThreeforTwo();
+    SumOfBill bill = new SumOfBill();
+    public boolean printShoppingList() {
+        int len = shoppingList.shoppingArr.size();
+        if(len > 0) {
+            printShoppingList(len);
+            return true;
+        }
+        return false;
+    }
+
+    private void printShoppingList(int len) {
+        System.out.println("```");
+        System.out.println("***<没钱赚商店>购物清单***");
+        for(int i = 0; i < len; i++) {
+            ShoppingListAll.Shopping oneShoppingRecord = shoppingList.shoppingArr.get(i);
+            printOneShoppingRecord(oneShoppingRecord);
+        }
+        System.out.println("----------------------");
+    }
+
+    private void printOneShoppingRecord(ShoppingListAll.Shopping oneShoppingRecord) {
+        System.out.print("名称："+oneShoppingRecord.name+"，");
+        System.out.print("数量："+oneShoppingRecord.count+oneShoppingRecord.unit+"，");
+        System.out.print("单价："+String.format("%.2f", oneShoppingRecord.price)+"(元)，");
+        System.out.print("小计："+String.format("%.2f", oneShoppingRecord.subtotal)+"(元)");
+        if( oneShoppingRecord.sale95 != 0.0 ) {
+            System.out.println(",节省："+String.format("%.2f", oneShoppingRecord.sale95)+"(元)");
+        }
+        else  {
+            System.out.println();
+        }
+    }
+
+    public boolean printThreeforTwo() {
+        int len = threeforTwo.threeforTwoArr.size();
+        if( len > 0 ) {
+            printThreeforTwo(len);
+            return true;
+        }
+        return false;
+    }
+
+    private void printThreeforTwo(int len) {
+        System.out.println("买二赠一商品：");
+        for(int i = 0; i < len; i++) {
+            PrivilegeThreeforTwo.ThreeforTwo oneThreeforTwoRecord = threeforTwo.threeforTwoArr.get(i);
+            printOneThreeforTwoRecord(oneThreeforTwoRecord);
+        }
+        System.out.println("----------------------");
+    }
+
+    private void printOneThreeforTwoRecord(PrivilegeThreeforTwo.ThreeforTwo oneThreeforTwoRecord) {
+        System.out.print("名称："+oneThreeforTwoRecord.name+"，");
+        System.out.println("数量："+oneThreeforTwoRecord.count+oneThreeforTwoRecord.unit);
+    }
+
+    public boolean printSumOfBill() {
+        System.out.println("总计："+String.format("%.2f",bill.total)+"(元)");
+        System.out.println( bill.privilege > 0.0 ? "节省："+String.format("%.2f",bill.privilege)+"(元)": "");
+        System.out.println("**********************");
+        System.out.println("```");
+        return true;
+    }
+
 }
--- src/test/java/Presenter/OutPutBillTest.java
+++ src/test/java/Presenter/OutPutBillTest.java
@@ -1,10 +1,76 @@
 package Presenter;
 
+import Model.PrintBillDetails.PrivilegeThreeforTwo;
+import Model.PrintBillDetails.ShoppingListAll;
+import Model.PrintBillDetails.SumOfBill;
+import org.junit.After;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by feng on 2016/7/19.
  */
 public class OutPutBillTest {
+
+    ShoppingListAll shoppingList = new ShoppingListAll();
+    PrivilegeThreeforTwo threeforTwo = new PrivilegeThreeforTwo();
+    SumOfBill bill = new SumOfBill();
+    OutPutBill printBill = new OutPutBill();
+
+    private ShoppingListAll.Shopping addOneBillingRecord(String name, int count, String unit, double price, double subtotal, double sale95) {
+
+        ShoppingListAll.Shopping oneBillingRecord = new ShoppingListAll.Shopping();
+        oneBillingRecord.name = name ;
+        oneBillingRecord.count = count ;
+        oneBillingRecord.price = price ;
+        oneBillingRecord.unit = unit ;
+        oneBillingRecord.subtotal = subtotal ;
+        oneBillingRecord.sale95 = sale95 ;
+        return oneBillingRecord ;
+
+    }
+
+    private PrivilegeThreeforTwo.ThreeforTwo addOneThreeforTwoRecord(String name, int count, String unit) {
+        PrivilegeThreeforTwo.ThreeforTwo oneThreeforTwoRecord = new PrivilegeThreeforTwo.ThreeforTwo();
+        oneThreeforTwoRecord.name = name;
+        oneThreeforTwoRecord.count = count;
+        oneThreeforTwoRecord.unit = unit;
+        return oneThreeforTwoRecord;
+    }
+
+    @After
+    public void reset_bill_data() {
+        ResetBillEmpty reset = new ResetBillEmpty();
+        reset.resetAll();
+    }
+
+    @Test
+    //购物清单当中有买二赠一的商品
+    public void should_print_shopping_list_when_Goods_have_commodity_of_three_for_two() {
+        //Given
+        shoppingList.shoppingArr.add(addOneBillingRecord("可口可乐",3,"瓶",3.00,6.00,0.0));
+        shoppingList.shoppingArr.add(addOneBillingRecord("羽毛球",5,"个",1.00,4.00,0.0));
+        shoppingList.shoppingArr.add(addOneBillingRecord("苹果",2,"斤",5.50,11.00,0.0));
+        threeforTwo.threeforTwoArr.add(addOneThreeforTwoRecord("可口可乐",1,"瓶"));
+        threeforTwo.threeforTwoArr.add(addOneThreeforTwoRecord("羽毛球",1,"个"));
+        bill.total = 21.00;
+        bill.privilege = 4.00;
+
+        //when
+        boolean shoppingListStatus = printBill.printShoppingList();
+        boolean threeforTwoStatus = printBill.printThreeforTwo();
+        boolean billSum = printBill.printSumOfBill();
+
+        //then
+        assertThat(shoppingListStatus, is(true));
+        assertThat(threeforTwoStatus, is(true));
+        assertThat(billSum, is(true));
+    }
+
+
+
+
 
 }
